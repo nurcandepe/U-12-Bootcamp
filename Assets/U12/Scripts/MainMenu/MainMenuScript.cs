b1f3442080d75ee23db0e3359ab6b677aaab1cb8
Onur Yılmaz <onuryilmazpw@gmail.com>
2023-07-15 12:02:46 +0300

    Main menü ayarlandı

--- Assets/U12/Scripts/MainMenu/MainMenuScript.cs
+++ Assets/U12/Scripts/MainMenu/MainMenuScript.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    //Sahnelere aktarýlacak bilgiler
+    public SOValues values;
+    //Paneller
     private GameObject playPanel;
-    private GameObject creditsPanel;
     private GameObject settingsPanel;
+    private GameObject creditsPanel;
+    //Butonlar
+    public Button playButton;
+    public Button manButton;
+    public Button womanButton;
+    public Button settingsButton;
+    public Button creditsButton;
+    public Button exitButton;
+    
 
     void Start()
     {
         playPanel = GameObject.Find("PlayPanel");
-        creditsPanel = GameObject.Find("CreditsPanel");
         settingsPanel = GameObject.Find("SettingsPanel");
+        creditsPanel = GameObject.Find("CreditsPanel");
 
-        creditsPanel.SetActive(false);
+        playPanel.SetActive(false);
         settingsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
     }
 
     void Update()
@@ -24,8 +37,55 @@ public class MainMenuScript : MonoBehaviour
         
     }
 
-    public void StartGameFunc()
+    private void OnEnable()
     {
+        playButton.onClick.AddListener(EnablePlayPanel);
+        manButton.onClick.AddListener(StartGameFuncMan);
+        womanButton.onClick.AddListener(StartGameWoman);
+        settingsButton.onClick.AddListener(EnableSettingsPanel);
+        creditsButton.onClick.AddListener(EnableCreditsPanel);
+        exitButton.onClick.AddListener(ExitGameFunc);
+    }
+
+    private void StartGameFuncMan()
+    {
+        values.gender = "male";
+        values.playerName = "Victor";
+        values.map = 10;
+        values.quest = 10;
         SceneManager.LoadScene("Base");
     }
+    private void StartGameWoman()
+    {
+        values.gender = "female";
+        values.playerName = "Valeria";
+        values.map = 10;
+        values.quest = 10;
+        SceneManager.LoadScene("Base");
+    }
+
+    private void ExitGameFunc()
+    {
+        Application.Quit();
+    }
+
+    private void EnablePlayPanel()
+    {
+        playPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+    private void EnableSettingsPanel()
+    {
+        playPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+    }
+    private void EnableCreditsPanel()
+    {
+        playPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
 }
