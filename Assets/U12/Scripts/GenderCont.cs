ad56cf112d1b0dfddf9e676af6c57671f70f2d76
Onur Yılmaz <onuryilmazpw@gmail.com>
2023-07-16 12:16:19 +0300

    Cinsiyet çalışması aktif hale getirildi

---
+++ Assets/U12/Scripts/GenderCont.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenderCont : MonoBehaviour
+{
+    public SOValues values;
+
+    private GameObject male;
+    private GameObject female;
+
+
+    void Start()
+    {
+        female = GameObject.Find("Player_Women");
+        male = GameObject.Find("Player_Men");
+
+        if (values.gender == "male")
+        {
+            //Bir hatadan dolayý ikisini kapatýp birini açýnca düzgün çalýþýyor
+            female.SetActive(false);
+            male.SetActive(false);
+
+            male.SetActive(true);
+        }
+        else
+        {
+            //Bir hatadan dolayý ikisini kapatýp birini açýnca düzgün çalýþýyor
+            female.SetActive(false);
+            male.SetActive(false);
+
+            female.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
